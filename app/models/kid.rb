f113987f37754d817ea611b4eb079a321ef83717
Jerome Michel <micl.jerome@gmail.com>
2020-06-10 16:17:52 +0200

    Add validations, relations and models

--- app/models/kid.rb
+++ app/models/kid.rb
@@ -1,5 +1,5 @@
 class Kid < ApplicationRecord
-  belongs_to :user
+  belongs_to :parent, class_name: 'User', foreign_key: 'user_id'
   has_many :babysits
   has_many :events, through: :babysits
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_10_110430) do
+ActiveRecord::Schema.define(version: 2020_06_10_130440) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -57,8 +57,9 @@ ActiveRecord::Schema.define(version: 2020_06_10_110430) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "first_name"
     t.string "last_name"
-    t.boolean "is_nanny"
+    t.boolean "is_nanny", default: false
     t.string "address"
+    t.string "phone_number"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
