791f2854e34309c356bc0cfc7f279c440c959609
Guillaume Belie <guillaume.belie.boisseau@gmail.com>
2020-06-10 22:54:51 +0200

    all kids profiles (index)

--- app/controllers/kids_controller.rb
+++ app/controllers/kids_controller.rb
@@ -1,4 +1,9 @@
 class KidsController < ApplicationController
+
+  def index
+    @kids = Kid.all
+  end
+
   def show
     @kid = Kid.find(params[:id])
   end
--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   get '/contact', to: 'pages#contact'
   get '/dashboard', to: 'pages#dashboard'
 
-  resources :kids, only: [:create, :show, :edit, :update]
+  resources :kids, only: [:index, :create, :show, :edit, :update]
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
