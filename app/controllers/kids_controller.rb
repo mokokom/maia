95b452ed2edd63f429e473290a53cb29ae6ecef1
Guillaume Belie <guillaume.belie.boisseau@gmail.com>
2020-06-10 22:23:15 +0200

    show kid

--- app/controllers/kids_controller.rb
+++ app/controllers/kids_controller.rb
@@ -14,6 +14,21 @@ class KidsController < ApplicationController
     end
   end
 
+  def edit
+    @kid = Kid.find(params[:id])
+  end
+
+def update
+  @kid = Kid.find(params[:id])
+  @kid.update(kid_param)
+  redirect_to kid_path
+    if @item.save
+      redirect_to item_path
+    else
+      render :new
+    end
+end
+
   private
 
   def kid_param
