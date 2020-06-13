274e3fa953486b909de2d8df5a5dc8b96fce05cb
Guillaume Belie <guillaume.belie.boisseau@gmail.com>
2020-06-13 21:31:21 +0200

    cloudinary fixes + validations dates for babysits

--- app/models/babysit.rb
+++ app/models/babysit.rb
@@ -2,4 +2,6 @@ class Babysit < ApplicationRecord
   belongs_to :kid
   belongs_to :nanny, class_name: 'User', foreign_key: 'user_id'
   has_many :events, dependent: :destroy
+
+  validates :begin_date, :end_date, presence: true
 end
--- app/models/event.rb
+++ app/models/event.rb
@@ -1,7 +1,5 @@
 class Event < ApplicationRecord
   belongs_to :babysit
-
   has_one_attached :photo
-
   validates :tag, :content, :begin_event, :end_event, presence: true
 end
