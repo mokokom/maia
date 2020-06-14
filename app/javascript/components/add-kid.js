1fc2ca7d5b56a5cb0801214e95f6c373877749c7
Raphael Delorme <mokokebeloke@gmail.com>
2020-06-14 10:30:13 +0200

    add condition an add-kid.js to avoid console error with addKid nil

--- app/javascript/components/add-kid.js
+++ app/javascript/components/add-kid.js
@@ -2,14 +2,16 @@ const toggleAddKidForm = () => {
   const nounouForms = document.querySelectorAll(".nounou-form");
   const addKid = document.querySelector(".add-kid");
   const addKidForm = document.querySelector(".add-kid-form");
-  addKid.addEventListener("click", () => {
-    nounouForms.forEach(nounouForm => {
-      nounouForm.classList.add("d-none");
+  if (addKid) {
+    addKid.addEventListener("click", () => {
+      nounouForms.forEach(nounouForm => {
+        nounouForm.classList.add("d-none");
+      });
+      $(addKidForm).toggleClass("d-none");
+      // addKidForm.style.display =
+      //   addKidForm.style.display === "none" ? "block" : "none";
     });
-    $(addKidForm).toggleClass("d-none");
-    // addKidForm.style.display =
-    //   addKidForm.style.display === "none" ? "block" : "none";
-  });
+  }
 };
 
 export { toggleAddKidForm };
