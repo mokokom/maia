fd7cd138315abf46d922e87937be5141b33657f4
Guillaume Belie <guillaume.belie.boisseau@gmail.com>
2020-06-17 13:40:57 +0200

    new event styling v1

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -28,6 +28,7 @@ import { stickyTagContainerOnScroll } from "../components/tags-container";
 import { scrollToTop } from "../components/scroll-to-top";
 import { toggleEventForm } from "../components/event-form";
 import { initCriterias } from "../components/filter-tag";
+import { eventTags } from "../components/event-tags";
 // Internal imports, e.g:
 // import { initSelect2 } from '../components/init_select2';
 
@@ -40,4 +41,5 @@ document.addEventListener("turbolinks:load", () => {
   scrollToTop();
   toggleEventForm();
   initCriterias();
+  eventTags();
 });
