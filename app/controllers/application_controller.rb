dfe7d6a806afa0ea97a17b57685c84432b69d4ff
Raphael Delorme <mokokebeloke@gmail.com>
2020-06-18 18:25:30 +0200

    add meta-tag

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   def default_url_options
-    { host: ENV["DOMAIN"] || "localhost:3000" }
+    { host: ENV["www.maia.guru"] || "localhost:3000" }
   end
 
   def after_sign_in_path_for(resource)
